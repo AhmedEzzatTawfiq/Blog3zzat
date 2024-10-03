a13a490bddd5a1ca0432bda1918f6b04063366d4
AhmedEzzatTawfiq <Ahmedezzat672001@gmail.com>
2024-10-03 06:21:21 -0700

    first commit

---
+++ app/components/dashboard/SubmitButtons.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Loader2 } from "lucide-react";
+import { useFormStatus } from "react-dom";
+
+interface iAppProps {
+  text: string;
+  className?: string;
+  variant?: "link" | "default" | "destructive" | "outline" | "secondary" | "ghost" | null | undefined;
+}
+
+export function SubmitButton({text, className, variant}: iAppProps) {
+  const {pending} = useFormStatus()
+  return (
+    <>
+      {pending ? (
+        <Button disabled className={cn("w-fit", className)} variant={variant}>
+          <Loader2 className="mr-2 size-4 animate-spin"/>
+        </Button>
+      ) : (
+        <Button type="submit" className={cn("w-fit", className)} variant={variant}>{text}</Button>
+      )}
+    </>
+  )
+}
---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,78 @@
+import Link from "next/link";
+import { ReactNode } from "react";
+import logo from "@/public/logos/logo.svg"
+import Image from "next/image";
+import { CircleUser, DollarSign, Globe, Home } from "lucide-react";
+import { DashboardItems } from "../components/dashboard/DashboardItems";
+import { ThemeToggle } from "../components/dashboard/ThemeToggle";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { Toaster } from "@/components/ui/sonner";
+
+export const navLinks = [
+  {
+    name: 'Dashboard',
+    href: '/dashboard',
+    icon: Home,
+  },
+  {
+    name: 'Sites',
+    href: '/dashboard/sites',
+    icon: Globe,
+  },
+  {
+    name: "Pricing",
+    href: "/dashboard/pricing",
+    icon: DollarSign,
+  }
+]
+
+
+export default function DashboardLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols[280px_1fr]">
+      <div className="hidden border-r bg-muted/40 md:block">
+        <div className="flex h-full max-h-screen flex-col gap-2">
+          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+            <Link href='/' className="flex items-center gap-2 font-semibold">
+              <Image src={logo} alt="logo" className="size-8" />
+              <h3 className="text-2xl">
+                Blog<span className="text-primary">3zzat</span>
+              </h3>
+            </Link>
+          </div>
+          <div className="flex-1 items-start px-2 font-medium lg:px-4">
+            <nav>
+              <DashboardItems />
+            </nav>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col">
+      <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+        <div className="ml-auto flex items-center gap-x-5">
+          <ThemeToggle />
+
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant='secondary' size='icon' className="rounded-full">
+                <CircleUser />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem asChild>
+                <LogoutLink>Logout</LogoutLink>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </header>
+      <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+        {children}
+        <Toaster richColors closeButton />
+      </main>
+      </div>
+    </div>
+  )
+}
