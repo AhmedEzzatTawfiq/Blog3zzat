ebd16ade733f357e8c138ad7aa43ae409f3a50b7
AhmedEzzatTawfiq <Ahmedezzat672001@gmail.com>
2024-10-03 09:23:58 -0700

    second commit

--- app/dashboard/sites/[siteId]/[articleId]/page.tsx
+++ app/dashboard/sites/[siteId]/[articleId]/page.tsx
@@ -5,13 +5,11 @@ import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 
-
 async function getData(postId: string) {
   const data = await prisma.post.findUnique({
     where: {
       id: postId,
     },
-
     select: {
       image: true,
       title: true,
@@ -22,20 +20,21 @@ async function getData(postId: string) {
     },
   });
 
-  if(!data) {
+  if (!data) {
     return notFound();
   }
 
   return data;
 }
 
-export default async function EditRoute({params}: {params: {articlId: string; siteId: string}}) {
-
-  const data = await getData(params.articlId);
-
+export default async function EditRoute({
+  params,
+}: {
+  params: { articleId: string; siteId: string };
+}) {
+  const data = await getData(params.articleId);
   return (
-  
-      <div>
+    <div>
       <div className="flex items-center">
         <Button size="icon" variant="outline" asChild className="mr-3">
           <Link href={`/dashboard/sites/${params.siteId}`}>
@@ -47,7 +46,5 @@ export default async function EditRoute({params}: {params: {articlId: string; si
 
       <EditArticleForm data={data} siteId={params.siteId} />
     </div>
-    
-  )
-
+  );
 }
